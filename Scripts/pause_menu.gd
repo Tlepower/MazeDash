993c5ad8d8affbf3a30832d045fb29ba45d33dce
Tlepower <danielakpan140@gmail.com>
2025-09-27 15:42:34 -0600

    changed control and fixed Pause

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,20 +1,22 @@
 extends Control
 
 func _ready():
-	$AnimationPlayer.play("RESET")
+	$AnimationPlayer.play("blur")
 
 func resume():
+	$".".hide()
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 	
 func pause():
+	$".".show()
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 	
 func testEsc():
-	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+	if Input.is_action_just_pressed("space") and get_tree().paused == false:
 		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+	elif Input.is_action_just_pressed("space") and get_tree().paused == true:
 		resume()
 
 func _on_resume_pressed():
