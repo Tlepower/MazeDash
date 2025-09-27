e7be829ec424750b0c2a7dfb0ca83b82b84443ff
Tlepower <danielakpan140@gmail.com>
2025-09-26 22:16:12 -0600

    fix movement

---
+++ Scripts/player_1.gd
@@ -0,0 +1,12 @@
+class_name PLAYER1
+
+extends CharacterBody2D
+
+@export var speed = 400
+
+func _physics_process(delta: float) -> void:
+	var input_direction = Input.get_vector("left","right","up","down")
+	
+	velocity = input_direction * speed
+	
+	move_and_slide()
