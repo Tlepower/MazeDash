663581d362168a733fc3bcce1844cc980f0b7818
unknown-wonder <rbassey03@gmail.com>
2025-09-27 14:52:08 -0600

    Main menu interactive buttons test - For now it just prints to the console

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# Called when the nore enters the scene for the first time
+func _ready():
+	pass
+
+# Called every frame
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	print("Play")
+
+
+func _on_controls_button_pressed() -> void:
+	print("Show Controls")
+
+
+func _on_shop_button_pressed() -> void:
+	print("Shop!!")
