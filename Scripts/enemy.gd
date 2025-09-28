f7fc208afb7ddd3034d75e32d378607208b2a9af
Tlepower <danielakpan140@gmail.com>
2025-09-28 14:05:55 -0600

    Made the enemy kill you on touch and it moves

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,3 +1,13 @@
 class_name Enemy
 
-extends CharacterBody2D
+extends Area2D
+
+func _ready() -> void:
+	position += Vector2(600,0)
+	connect("body_entered", _on_body_entered)
+	
+
+func _on_body_entered(body):
+	if body is PLAYER1:
+		print("Game over")
+		get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
