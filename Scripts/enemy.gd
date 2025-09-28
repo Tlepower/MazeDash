9bee4527b1694bff99db039079e1ea7d21333ed0
Tlepower <danielakpan140@gmail.com>
2025-09-28 14:13:02 -0600

    Updated enemy to not produce errors

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,10 +4,13 @@ extends Area2D
 
 func _ready() -> void:
 	position += Vector2(600,0)
-	connect("body_entered", _on_body_entered)
+	#connect("body_entered", _on_body_entered)
 	
 
 func _on_body_entered(body):
 	if body is PLAYER1:
 		print("Game over")
-		get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
+		call_deferred("end_game")
+
+func end_game():
+	get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
