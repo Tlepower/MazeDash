b7b983c0edcbfcfb62742fba2fd593a493abcdd7
Tlepower <danielakpan140@gmail.com>
2025-09-28 15:26:02 -0600

    the enemy kills and runs after you

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,11 @@
 class_name Enemy
 
-extends Area2D
+extends CharacterBody2D
 
-func _ready() -> void:
-	position += Vector2(600,0)
-	#connect("body_entered", _on_body_entered)
+@onready var target=$"../PLAYER1"
+var speed=350
+
 	
-
 func _on_body_entered(body):
 	if body is PLAYER1:
 		print("Game over")
@@ -14,3 +13,15 @@ func _on_body_entered(body):
 
 func end_game():
 	get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
+
+func _physics_process(_delta):
+	var direction=(target.position-position).normalized()
+	velocity=direction*speed
+	look_at(target.position)
+	move_and_slide()
+
+
+func _on_area_2d_for_enemy_body_entered(body) -> void:
+	if body is PLAYER1:
+		print("Game over")
+		call_deferred("end_game")
