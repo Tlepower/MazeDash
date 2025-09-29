d491981466ad48b13ccebaa7487c7e58ea500574
Richard Bassey (apb245) <apb245@mail.usask.ca>
2025-09-28 23:01:24 -0600

    Changed font for you win scene and controls scene

--- Scripts/you_win.gd
+++ Scripts/you_win.gd
@@ -1,7 +1,5 @@
 extends Control
 
 
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
-	
+	Global.go_to_scene("res://Scenes/Menus/Main_Menu.tscn")
